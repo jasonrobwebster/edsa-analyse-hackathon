2b030e677a2e17d1694ea7922c76a4bc6ea1a3c3
jasonrobwebster <jasonrobwebster@gmail.com>
2019-02-26 13:21:41 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='edsa_hackathon',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA hackathon model solution',
+    long_description=open('README.md').read(),
+    install_requires=[],
+    url='https://github.com/jasonrobwebster/edsa-analyse-hackathon',
+    author='Jason Webster',
+    author_email='jason@explore-ai.net'
+)
