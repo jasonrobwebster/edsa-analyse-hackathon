ebe4d0b2334e6986fba7261fe49379451a1fd31c
jasonrobwebster <jasonrobwebster@gmail.com>
2019-03-15 15:02:11 +0200

    Update recursions.py

--- hackathon/recursion/recursions.py
+++ hackathon/recursion/recursions.py
@@ -20,7 +20,9 @@ def factorial(n):
 def fibonacci(x):
     if x < 0:
         raise TypeError('Given value is less than 0!')
-    if x == 0 or x==1:
+    if x == 0:
+        return 0
+    elif x == 1:
         return 1
     else:
         return fibonacci(x-1) + fibonacci(x-2)
