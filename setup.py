fd6e65035a13bf8d4f49c87ccde08af7dbc31157
jasonrobwebster <jasonrobwebster@gmail.com>
2019-03-15 14:56:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='edsa_hackathon',
+    name='edsa-hackathon',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
